d9f80724506b35b25300e94639a5be6c2400a6ba
rohitudas <43554634+rohitudas@users.noreply.github.com>
2018-12-17 23:07:45 -0800

    commit

---
+++ CodeFirstEF1/CodeFirstEF1/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstEF1.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string Height { get; set; }
+        public string Weight { get; set; }
+        public Grade Grade { get; set; }
+    }
+}
---
+++ CodeFirstEF1/CodeFirstEF1/View/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeFirstEF1.DAL;
+using CodeFirstEF1.Models;
+
+namespace CodeFirstEF1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var db = new SchoolContext())
+            {
+                string check = "";
+
+                while (check != "no")
+                {
+                    var temp = new Grade();
+                    var stud = new Student();
+                    Console.WriteLine("Enter New Student");
+                    Console.Write("Name:");
+                    stud.StudentName = Console.ReadLine();
+                    Console.Write("Height:");
+                    stud.Height = Console.ReadLine();
+                    Console.Write("Weight:");
+                    stud.Weight = Console.ReadLine();
+                    Console.Write("What Grade:");
+                    temp.GradeName = Console.ReadLine(); 
+                    Console.Write("Section:");
+                    temp.Section = Console.ReadLine();
+                    Console.WriteLine("Saving to db....");
+                    db.Students.Add(stud);
+                    db.Grades.Add(temp);
+                    db.SaveChanges();
+                    Console.WriteLine("Success!");
+                    Console.WriteLine("Add another student?(yes or no)");
+                    check = Console.ReadLine();
+
+                }
+                foreach(var student in db.Students)
+                {
+                    Console.Write(student.StudentID + ". "+student.StudentName+" ");
+                    foreach(var grade in db.Grades)
+                    {
+                        if (student.StudentID == grade.GradeId)
+                        {
+                            Console.Write("Grade: "+grade.GradeName);
+                        }
+                    }
+                    Console.WriteLine();
+                    
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
